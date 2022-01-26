808a788960eca81efcad85d30aadff248a582022
ChaosBlaze <chandrashekar9479@gmail.com>
2022-01-25 23:22:28 -0500

    module 2 exercises

---
+++ Module2_Exercise1.py
@@ -0,0 +1,7 @@
+# Group Members: Ivory Steed, Paul Thottappilly, Hassan Chugtai, Tony Le, Shekar Balasubramaniam
+#https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+
+input = input("Enter a String: ")[::-1]
+
+print(input)
